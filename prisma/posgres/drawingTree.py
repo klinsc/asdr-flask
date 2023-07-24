ea5c86139bb7448651dc37e5d76539c199cc1196
klinsc <klinscea@gmail.com>
2023-07-24 18:25:54 +0700

    Update drawingTree.py

--- prisma/posgres/drawingTree.py
+++ prisma/posgres/drawingTree.py
@@ -54,7 +54,7 @@ drawingTree = {
                     "115_1way_ds_w_motor": 1,
                     "115_ds": 1,
                 },
-                "115_gs":1,
+                "115_gs": 1,
                 "115_la": 1,
                 "ss_man_mode": 1,
                 "DPM": 1,
@@ -70,87 +70,89 @@ drawingTree = {
                 "DIM": 1,
             }
         },
-        # "22_tie": {
-        #     "mandatory": {
-        #         "22_breaker": 1,
-        #         "vt": {
-        #             "22_vt_3p_x2": {
-        #                 "22_vt_3p": 2,
-        #                 "DPM": 1,
-        #                 "LL": 2,
-        #             }
-        #         }
-        #     },
-        # },
-        # "22_capacitor": {
-        #     "mandatory": {
-        #         "22_breaker": 1,
-        #         "22_cap_bank": 1,
-        #         "22_gs": 1,
-        #         "22_ll": 1,
-        #         "Q": 1,
-        #         "terminal": {
-        #             "terminal_w_future": 1,
-        #             "terminal_single": 1,
-        #         },
-        #     },
-        #     "optional": {
-        #         "DIM": 1,
-        #         "DPM": 1,
-        #     }
-        # },
-        # "22_outgoing": {
-        #     "mandatory": {
-        #         "22_breaker": 1,
-        #         "out": {
-        #             "22_ds_la_out": 1,
-        #             "22_ds_out": 1,
-        #         },
-        #         "22_gs": 1,
-        #         "22_ll": 1,
-        #         "terminal_single": 1,
-        #         "terminal": {
-        #             "terminal_w_future": 1,
-        #             "terminal_single": 1,
-        #             "terminal_double": 1,
-        #         },
-        #     },
-        #     "optional": {
-        #         "DIM": 1,
-        #         "DPM": 1,
-        #     }
-        # },
-        # "22_service": {
-        #     "mandatory": {
-        #         "22_ds": 1,
-        #         "22_ll": 1,
-        #         "22_gs": 1,
-        #         "terminal_single": 1,
-        #         "terminal": {
-        #             "terminal_w_future": 1,
-        #             "terminal_single": 1,
-        #             "terminal_double": 1,
-        #         },
-        #     },
-        # },
-        # "22_incoming": {
-        #     "mandatory": {
-        #         "22_breaker": 1,
-        #         "22_gs": 1,
-        #         "22_ll": 1,
-        #         "LL": 1,
-        #         "terminal": {
-        #             "terminal_single_x2": {
-        #                 "terminal_single": 2,
-        #             },
-        #             "terminal_double_x2": {
-        #                 "terminal_double": 2,
-        #             },
-        #         },
-        #     },
-        #     "optional": {
-        #         "DIM": 1,
-        #         "DPM": 1,
-        #     }
-        # }
+        "22_tie": {
+            "mandatory": {
+                "22_breaker": 1,
+                "vt": {
+                    "_total": 2,
+                    "22_vt_1p": 1,
+                    "22_vt_3p": 1,
+                    "22_cvt_1p": 1,
+                    "22_cvt_3p": 1,
+                },
+                "DPM": 1,
+                "LL": 2,
+            },
+        },
+        "22_capacitor": {
+            "mandatory": {
+                "22_breaker": 1,
+                "22_cap_bank": 1,
+                "22_gs": 1,
+                "22_ll": 1,
+                "Q": 1,
+                "terminator": {
+                    "_total": 1,
+                    "terminator_w_future": 1,
+                    "terminator_single": 1,
+                },
+            },
+            "optional": {
+                "DIM": 1,
+                "DPM": 1,
+            }
+        },
+        "22_outgoing": {
+            "mandatory": {
+                "22_breaker": 1,
+                "out": {
+                    "_total": 1,
+                    "22_ds_la_out": 1,
+                    "22_ds_out": 1,
+                },
+                "22_gs": 1,
+                "22_ll": 1,
+                "terminator": {
+                    "_total": 2,
+                    "terminator_w_future": 1,
+                    "terminator_single": 1,
+                    "terminator_double": 1,
+                },
+            },
+            "optional": {
+                "DIM": 1,
+                "DPM": 1,
+            }
+        },
+        "22_service": {
+            "mandatory": {
+                "22_ds": 1,
+                "22_ll": 1,
+                "22_gs": 1,
+                "terminator": {
+                    "_total": 2,
+                    "terminator_w_future": 1,
+                    "terminator_single": 1,
+                    "terminator_double": 1,
+                },
+            },
+        },
+        "22_incoming": {
+            "mandatory": {
+                "22_breaker": 1,
+                "22_gs": 1,
+                "22_ll": 1,
+                "LL": 1,
+                "v_m": 1,
+                "terminator": {
+                    "_total": 2,
+                    "terminator_single": 1,
+                    "terminator_double": 1,
+                },
+            },
+            "optional": {
+                "DIM": 1,
+                "DPM": 1,
+            }
+        }
     }}
