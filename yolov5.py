4f5a04e92ea12223aaf25f7e703a6fc96852f608
klinsc <klinscea@gmail.com>
2023-09-25 16:13:22 +0700

    Update yolov5.py

--- yolov5.py
+++ yolov5.py
@@ -1,14 +1,28 @@
+import os
+
 import torch
 
 
 class YoloV5:
     def __init__(self):
-        # Load the model
-        self.model = torch.hub.load(
-            "ultralytics/yolov5",
-            "custom",
-            path="./models/best_dc4c535796dc43b88f23fcc811b17a4c.pt",
-        )  # default
+        try:
+            self.file_name = "best_dc4c535796dc43b88f23fcc811b17a4c.pt"
+            assert self.file_name in os.listdir("./models"), "Model not found"
+
+            # Load the model
+            self.model = torch.hub.load(
+                "ultralytics/yolov5",
+                "custom",
+                path=f"./models/{self.file_name}",
+            )
+
+            # self.error = False
+            # self.error_message = None
+            self.error = {"error": False, "error_message": None}
+        # catch exception if model not found
+        except Exception as e:
+            print(e)
+            self.error = {"error": True, "error_message": "Error loading model"}
 
     # Function to predict the bounding boxes
     def predict(self, image_name=None):
