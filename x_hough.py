97eeb4de8faee33ae2f061eb59928fa6d6b112da
klinsc <klinscea@gmail.com>
2023-10-24 10:31:19 +0700

    check point (2)

--- x_hough.py
+++ x_hough.py
@@ -4,33 +4,33 @@ import numpy as np
 # Load image
 image = cv2.imread("./images/mrb-Manorom2-sm-bh.jpg")
 
-# Convert the image to gray scale
+# Convert image to grayscale
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# Perform edge detection
-edges = cv2.Canny(gray, 50, 200, apertureSize=3)
+# Use canny edge detection
+edges = cv2.Canny(gray, 50, 150, apertureSize=3)
 
-# Perform a Hough Line Transform
-lines = cv2.HoughLines(edges, 1, np.pi / 180, 300)
+# Apply HoughLinesP method to
+# to directly obtain line end points
+lines_list = []
+lines = cv2.HoughLinesP(
+    edges,  # Input edge image
+    1,  # Distance resolution in pixels
+    np.pi / 180,  # Angle resolution in radians
+    threshold=100,  # Min number of votes for valid line
+    minLineLength=5,  # Min allowed length of line
+    maxLineGap=10,  # Max allowed gap between line for joining them
+)
 
-# Iterate over the output lines and draw them on the image
-for rho, theta in lines[0]:
-    a = np.cos(theta)
-    b = np.sin(theta)
-    x0 = a * rho
-    y0 = b * rho
-    x1 = int(x0 + 1000 * (-b))
-    y1 = int(y0 + 1000 * (a))
-    x2 = int(x0 - 1000 * (-b))
-    y2 = int(y0 - 1000 * (a))
+# Iterate over points
+for points in lines:
+    # Extracted points nested in the list
+    x1, y1, x2, y2 = points[0]
+    # Draw the lines joing the points
+    # On the original image
+    cv2.line(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+    # Maintain a simples lookup list for points
+    lines_list.append([(x1, y1), (x2, y2)])
 
-    cv2.line(image, (x1, y1), (x2, y2), (255, 0, 0), 2)
-
-# Show result
-cv2.imshow("Result Image", image)
-
-# Save result
-cv2.imwrite("./images/mrb-Manorom2-sm-bh-hough.jpg", image)
-
-# Wait for key
-cv2.waitKey(0)
+# Save the result image
+cv2.imwrite("detectedLines.png", image)
